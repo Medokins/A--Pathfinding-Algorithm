d40ee27388095fc8a6e16a6f092047793dafe67a
Medokins <nikodemszwast@gmail.com>
2022-07-12 14:06:59 +0200

    add maze creation menu

--- maze.py
+++ maze.py
@@ -18,12 +18,23 @@ class Maze:
         self.y = 0
 
         # state
-        self.state = 'choosig'
+        self.state = 'choosing'
+
+        # maze variables
+        self.start_pos = None
+        self.end_pos = None
+        self.walls = []
     
     def draw(self):
         # set color to bg of maze
         screen.fill(BG_COLOR)
 
+        if self.start_pos != None:
+            pygame.draw.rect(screen, START, pygame.Rect(self.start_pos[0], self.start_pos[1], 50, 50))
+        if self.end_pos != None:
+            pygame.draw.rect(screen, END, pygame.Rect(self.end_pos[0], self.end_pos[1], 50, 50))
+        for wall in self.walls:
+            pygame.draw.rect(screen, WALL, pygame.Rect(wall[0], wall[1], 50, 50))
         # grid creation
         for x in range(0, 1000, 50):
             # horizontal lines
@@ -42,6 +53,21 @@ def runMaze():
             if event.type == pygame.QUIT:
                 maze.state = 'end'
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    maze.state = "choosing_start_pos"
+                if event.key == pygame.K_e:
+                    maze.state = "choosing_end_pos"
+                if event.key == pygame.K_w:
+                    maze.state = "creating_obstacles"
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if maze.state == "choosing_start_pos":
+                    maze.start_pos = (maze.x // 50 * 50, maze.y // 50 * 50)
+                if maze.state == "choosing_end_pos":
+                    maze.end_pos = (maze.x // 50 * 50, maze.y // 50 * 50)
+                if maze.state == "creating_obstacles":
+                    if (maze.x // 50 * 50, maze.y // 50 * 50) not in {maze.start_pos, maze.end_pos}:
+                        maze.walls.append((maze.x // 50 * 50 , maze.y // 50 * 50))
 
         try:
             maze.draw()
