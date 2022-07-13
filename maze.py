5fe70b0b60e79e5a3d4d35b7eba8d3ac3d68062c
Medokins <nikodemszwast@gmail.com>
2022-07-13 13:16:26 +0200

    add customisable grid size and maze_nodes creation

--- maze.py
+++ maze.py
@@ -1,15 +1,22 @@
 import pygame
 import numpy as np
+from node import Node
 
 pygame.init()
 WINDOW_SIZE = (1000, 1000)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Maze Creator")
 
+SQUARE_SIZE = 50
+
 BG_COLOR = (253, 244, 220)
 START = (0, 255, 0)
 END = (255, 0, 0)
 WALL = (0, 0, 0)
+
+AVAILABLE = (118, 187, 104)
+PATHED = (249, 56, 34)
+FINAL_PATH = (144, 224, 247)
 
 class Maze:
     def __init__(self):
@@ -30,13 +37,13 @@ class Maze:
         screen.fill(BG_COLOR)
 
         if self.start_pos != None:
-            pygame.draw.rect(screen, START, pygame.Rect(self.start_pos[0], self.start_pos[1], 50, 50))
+            pygame.draw.rect(screen, START, pygame.Rect(self.start_pos[0], self.start_pos[1], SQUARE_SIZE, SQUARE_SIZE))
         if self.end_pos != None:
-            pygame.draw.rect(screen, END, pygame.Rect(self.end_pos[0], self.end_pos[1], 50, 50))
+            pygame.draw.rect(screen, END, pygame.Rect(self.end_pos[0], self.end_pos[1], SQUARE_SIZE, SQUARE_SIZE))
         for wall in self.walls:
-            pygame.draw.rect(screen, WALL, pygame.Rect(wall[0], wall[1], 50, 50))
+            pygame.draw.rect(screen, WALL, pygame.Rect(wall[0], wall[1], SQUARE_SIZE, SQUARE_SIZE))
         # grid creation
-        for x in range(0, 1000, 50):
+        for x in range(0, WINDOW_SIZE[0], SQUARE_SIZE):
             # horizontal lines
             pygame.draw.line(screen, color = (0,0,0), start_pos = (0, x), end_pos = (WINDOW_SIZE[0], x))
             # vertical lines
@@ -45,15 +52,18 @@ class Maze:
         pygame.display.update()
 
 def runMaze():
+    # maze creation
     maze = Maze()
-    while maze.state != 'end':
+    while maze.state != 'ready':
         maze.x, maze.y = pygame.mouse.get_pos()
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 maze.state = 'end'
                 pygame.quit()
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    if maze.start_pos != None and maze.end_pos != None:
+                        maze.state = 'ready'
                 if event.key == pygame.K_s:
                     maze.state = "choosing_start_pos"
                 if event.key == pygame.K_e:
@@ -62,14 +72,31 @@ def runMaze():
                     maze.state = "creating_obstacles"
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if maze.state == "choosing_start_pos":
-                    maze.start_pos = (maze.x // 50 * 50, maze.y // 50 * 50)
+                    maze.start_pos = (maze.x // SQUARE_SIZE * SQUARE_SIZE, maze.y // SQUARE_SIZE * SQUARE_SIZE)
                 if maze.state == "choosing_end_pos":
-                    maze.end_pos = (maze.x // 50 * 50, maze.y // 50 * 50)
+                    maze.end_pos = (maze.x // SQUARE_SIZE * SQUARE_SIZE, maze.y // SQUARE_SIZE * SQUARE_SIZE)
                 if maze.state == "creating_obstacles":
-                    if (maze.x // 50 * 50, maze.y // 50 * 50) not in {maze.start_pos, maze.end_pos}:
-                        maze.walls.append((maze.x // 50 * 50 , maze.y // 50 * 50))
-
+                    if (maze.x // SQUARE_SIZE * SQUARE_SIZE, maze.y // SQUARE_SIZE * SQUARE_SIZE) not in {maze.start_pos, maze.end_pos}:
+                        maze.walls.append((maze.x // SQUARE_SIZE * SQUARE_SIZE , maze.y // SQUARE_SIZE * SQUARE_SIZE))
         try:
             maze.draw()
         except:
             print("Display turned off")
+        
+    # path finding   
+    maze_nodes = []
+    for x in range(0, WINDOW_SIZE[0], SQUARE_SIZE):
+        for y in range(0, WINDOW_SIZE[1], SQUARE_SIZE):
+            if (x,y) not in maze.walls:
+                maze_nodes.append(Node(coordinates = (x,y), walkable = True))
+            else:
+                maze_nodes.append(Node(coordinates = (x,y), walkable = False))
+
+    open = []
+    closed = []
+
+    print(len(maze_nodes))
+
+    found_path = False
+    # while not found_path:
+    #     pass
