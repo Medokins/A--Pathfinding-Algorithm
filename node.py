e4addfc3d7cd74800d0cbb625f6ed4eb35daf03f
Medokins <nikodemszwast@gmail.com>
2022-07-14 16:35:29 +0200

    create get_F_cost method

--- node.py
+++ node.py
@@ -1,3 +1,5 @@
+from settings import *
+
 class Node:
     def __init__(self, coordinates, walkable, weight = 0):
         # wall or path
@@ -10,8 +12,12 @@ class Node:
         self.G_cost = 0
         # distance from end node
         self.H_cost = 0
-        # summ of G_cost nad H_cost
-        self.F_cost = self.G_cost + self.H_cost
 
         # color of node
-        self.color = None
+        if walkable:
+            self.color = BG_COLOR
+        else:
+            self.color = WALL
+    
+    def get_F_cost(self):
+        return self.G_cost + self.H_cost
