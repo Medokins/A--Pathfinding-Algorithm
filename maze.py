4411e975066d52f67e8874c8cc3a7e62f40d4707
Medokins <nikodemszwast@gmail.com>
2022-07-15 11:38:50 +0200

    add getNeighbours function and change maze_nodes structure

--- maze.py
+++ maze.py
@@ -1,9 +1,9 @@
 import pygame
+import numpy as np
 from node import Node
 from settings import *
 
 pygame.init()
-WINDOW_SIZE = (1000, 1000)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Maze Creator")
 
@@ -40,8 +40,27 @@ class Maze:
 
         pygame.display.update()
 
+def getNeighbours(node, maze_nodes):
+    neighbours = []
+
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                pass
+            else:
+                checkX = node.x + x
+                checkY = node.y + y
+
+                if (checkX >= 0 and checkX < ARRAY_SIZE[0]) and (checkY >= 0 and checkY < ARRAY_SIZE[1]):
+                    if maze_nodes[checkX][checkY].walkable:
+                        neighbours.append(Node(coordinates = (checkX, checkY), walkable = True))
+                    else:
+                        neighbours.append(Node(coordinates = (checkX, checkY), walkable = False))
+
+    return neighbours
+
 def runMaze():
-    # maze creation
+    ######################################################## maze creation
     maze = Maze()
     while maze.state != 'ready':
         maze.x, maze.y = pygame.mouse.get_pos()
@@ -72,18 +91,19 @@ def runMaze():
         except:
             print("Display turned off")
         
-    # path finding   
-    maze_nodes = []
-    for x in range(0, WINDOW_SIZE[0], SQUARE_SIZE):
-        for y in range(0, WINDOW_SIZE[1], SQUARE_SIZE):
-            if (x,y) == maze.start_pos:
-                start_node = Node((x/SQUARE_SIZE,y/SQUARE_SIZE), walkable = True)
-            elif (x,y) == maze.end_pos:
-                target_node = Node((x/SQUARE_SIZE,y/SQUARE_SIZE), walkable = True)
-            elif (x,y) not in maze.walls:
-                maze_nodes.append(Node(coordinates = (x/SQUARE_SIZE,y/SQUARE_SIZE), walkable = True))
+    ######################################################## path finding
+    maze_nodes = np.empty((ARRAY_SIZE), dtype = Node)
+    # creating grid of nodes
+    for x in range(int(WINDOW_SIZE[0] / SQUARE_SIZE)):
+        for y in range(int(WINDOW_SIZE[1] / SQUARE_SIZE)):
+            if (x*SQUARE_SIZE,y*SQUARE_SIZE) == maze.start_pos:
+                start_node = Node((x,y), walkable = True)
+            elif (x*SQUARE_SIZE,y*SQUARE_SIZE) == maze.end_pos:
+                target_node = Node((x,y), walkable = True)
+            elif (x*SQUARE_SIZE,y*SQUARE_SIZE) not in maze.walls:
+                maze_nodes[x][y] = Node(coordinates = (x,y), walkable = True)
             else:
-                maze_nodes.append(Node(coordinates = (x/SQUARE_SIZE,y/SQUARE_SIZE), walkable = False))
+                maze_nodes[x][y] = Node(coordinates = (x,y), walkable = False)
 
     open_set = [start_node]
     closed_set = []
@@ -93,10 +113,13 @@ def runMaze():
         for node in open_set[1:]:
             if (node.get_F_cost() < current_node.get_F_cost()) or (node.get_F_cost() == current_node.get_F_cost() and node.H_cost < current_node.H_cost):
                 current_node = node
-        open_set.pop(current_node)
+
+        open_set.remove(current_node)
         closed_set.append(current_node)
 
         if current_node == target_node:
             quit()
-        else:
-            pass
+
+        for neighbour in getNeighbours(current_node, maze_nodes):
+            if (not neighbour.walkable) or (neighbour in closed_set):
+                pass
