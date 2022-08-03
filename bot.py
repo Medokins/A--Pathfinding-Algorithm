653863ef65946683b8b7b23eca4bf95e40e12039
Medokins <nikodemszwast@gmail.com>
2022-08-03 16:25:56 +0200

    update according to update in runMaze function

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ from maze import runMaze
 # if you want to draw your own maze, leave maze_file as None:
 # maze_file = None
 
-maze_file = "maze_1"
-path = runMaze(maze_file)
+path = runMaze()
 
 for node in path:
     print(node.x, node.y)
