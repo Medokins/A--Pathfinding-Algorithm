e93733a3e894b827541dc5bb460ed6c09ef143d7
Medokins <nikodemszwast@gmail.com>
2022-08-05 18:45:52 +0200

    add getDistance function and change main loop

--- bot.py
+++ bot.py
@@ -1,27 +1,56 @@
 from maze import runMaze
 from maze_functions import get_instructions
-import pydirectinput as pdi
+import pydirectinput
+import time
 
-def moveRight():
-    pass
-def moveLeft():
-    pass
-def moveDown():
-    pass
-def moveUp():
-    pass
+WALK_SPEED = 4.317
+
+def moveRight(duration):
+    pydirectinput.keyDown("d")
+    time.sleep(duration/WALK_SPEED)
+    pydirectinput.keyUp("d")
+def moveLeft(duration):
+    pydirectinput.keyDown("a")
+    time.sleep(duration/WALK_SPEED)
+    pydirectinput.keyUp("a")
+def moveDown(duration):
+    pydirectinput.keyDown("s")
+    time.sleep(duration/WALK_SPEED)
+    pydirectinput.keyUp("s")
+def moveUp(duration):
+    pydirectinput.keyDown("w")
+    time.sleep(duration/WALK_SPEED)
+    pydirectinput.keyUp("w")
 
 path = runMaze()
 instructions = get_instructions(path)
+time.sleep(3)
 
-for instruction in instructions:
+# for smoother walk
+def getDistance(instructions):
+    counter = 0
+    current_character = instructions[counter]
+
+    if instructions[:-1] == instructions[1:]:
+        return current_character, len(instructions)
+    else:
+        while current_character == instructions[counter]:
+            counter += 1
+        
+    return current_character, counter
+
+while len(instructions) > 0:
+    instruction, duration = getDistance(instructions)
+
     if instruction == 'R':
-        moveRight()
+        moveRight(duration)
     elif instruction == 'L':
-        moveLeft()
+        moveLeft(duration)
     elif instruction == 'D':
-        moveDown()
+        moveDown(duration)
     elif instruction == 'U':
-        moveUp()
+        moveUp(duration)
     else:
         print("Unknown instruction")
+    
+    instructions = instructions[duration:]
