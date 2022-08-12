59238528a199598d3a5831edd0dd16fb0370dd6b
Medokins <nikodemszwast@gmail.com>
2022-08-12 21:03:30 +0200

    add 3d mazes creation

--- minecraft/get_grid.py
+++ minecraft/get_grid.py
@@ -9,11 +9,13 @@ from minecraft_settings import *
 # run this file from project's main directory not from minecraft directory!
 mc = Minecraft.create()
 x, y, z = mc.player.getPos()
+maze_3d_name = "3d_maze"
+maze_2d_name = "minecraft_maze"
 
 # if facing east it will create paramiter to the east and south of current position
 if set_paramiter:
-    mc.setBlocks(x, y - 1, z, x + LENGHT, y - 1 + HEIGHT, z + WIDTH, block.AIR.id)
-    mc.setBlocks(x, y - 1, z, x + LENGHT, y, z + WIDTH, block.STONE.id)
+    mc.setBlocks(x, y - 1, z, x + LENGHT - 1, y - 1 + HEIGHT, z + WIDTH -1 , block.AIR.id)
+    mc.setBlocks(x, y - 1, z, x + LENGHT - 1, y - 1, z + WIDTH - 1, block.STONE.id)
 else:
     x = np.floor(x)
     y = np.floor(y)
@@ -38,9 +40,14 @@ start_node = None
 target_node = None
 layer_counter = 0
 
-df = pd.DataFrame(index=range(LENGHT), columns=range(WIDTH))
+if HEIGHT > 1:
+    try:
+        os.mkdir(os.path.join("3d_mazes", maze_3d_name))
+    except FileExistsError:
+        print("Directory with that name already exists")
 
 for layer in grid:
+    df = pd.DataFrame(index=range(LENGHT), columns=range(WIDTH))
     row_counter = 0
     for row in layer:
         block_counter = 0
@@ -54,10 +61,15 @@ for layer in grid:
                     target_node = (block_counter, LENGHT - row_counter - 1)
                 elif mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).data == 15:
                     df[block_counter][LENGHT - row_counter - 1] = '-'
+                elif mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).id != 0:
+                    # leaving it as wall for now, will change to correct weight in the future
+                    df[block_counter][LENGHT - row_counter - 1] = '-'
             block_counter += 1
         row_counter += 1
-    layer_counter += 1
-
-if start_node != None and target_node != None:
+    
     df.fillna(0, inplace=True)
-    df.to_csv(os.path.join("saved_mazes", "minecraft_maze.csv"))
+    if HEIGHT > 1:
+        df.to_csv(os.path.join("3d_mazes", maze_3d_name, f"layer_{layer_counter}.csv"))
+    else:
+         df.to_csv(os.path.join("saved_mazes", f"{maze_2d_name}.csv"))
+    layer_counter += 1
