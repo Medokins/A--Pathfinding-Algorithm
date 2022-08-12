78577f36a1f8352a864ae759d0b9f86ce5d0cc8b
Medokins <nikodemszwast@gmail.com>
2022-08-12 21:00:57 +0200

    change height to 5

--- minecraft/minecraft_settings.py
+++ minecraft/minecraft_settings.py
@@ -1,9 +1,10 @@
-WIDTH = 10
-LENGHT = 10
-HEIGHT = 0
+WIDTH = 5
+LENGHT = 5
+HEIGHT = 5
 
 START = (251, 5)
 END = (251, 14)
 WALL = (251, 15)
+PATH = (251, 3)
 MARGIN = 0.545
 set_paramiter = False
