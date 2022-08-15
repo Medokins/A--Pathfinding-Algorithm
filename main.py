c369ec37a4c94ee5cad44365cf866a6cdbec1fdc
Medokins <nikodemszwast@gmail.com>
2022-08-15 16:01:21 +0200

    add final path output to console

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from maze import runMaze
+from maze_functions import get_instructions
 
 if __name__ == "__main__":
-    runMaze()
+    path = runMaze()
+    for move in get_instructions(path):
+        print(move, end = " | ")
