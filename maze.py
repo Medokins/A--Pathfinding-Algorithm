01f6c3144bebbb515997544f6e832baabc523863
Medokins <nikodemszwast@gmail.com>
2022-08-22 13:00:45 +0200

    add partially working path finding with player_available path

--- maze.py
+++ maze.py
@@ -329,7 +329,7 @@ def runMaze():
             closed_set = []
       
             # modify maze_nodes to find walkable by player path
-            # this turned to be much harder than I thought it'll be, this one does not work yet, but im making progress
+            # this turned to be much harder than I thought it'll be, this one does work, but target node doesn't have parent node for some reason so It cannot retract path
             if PLAYER_AVAILABLE_PATH:
                 start_node.walkable = False
                 target_node.walkable = False
