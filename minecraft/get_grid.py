8e017ff5f1bc897ba99613520a93eb553827802d
Medokins <nikodemszwast@gmail.com>
2022-09-17 13:02:07 +0200

    add first step to impementing improved (faster) get_grid

--- minecraft/get_grid.py
+++ minecraft/get_grid.py
@@ -5,11 +5,12 @@ import pandas as pd
 import os
 
 from minecraft_settings import *
+from get_blocks import get_blocks
 
-# run this file from project's main directory not from minecraft directory!
+#run this file from project's main directory not from minecraft directory!
 mc = Minecraft.create()
 x, y, z = mc.player.getPos()
-maze_3d_name = "3d_maze"
+maze_3d_name = "big_3d_maze"
 maze_2d_name = "minecraft_maze"
 
 # if facing east it will create paramiter to the east and south of current position
@@ -22,17 +23,9 @@ else:
     z = np.floor(z)
     mc.player.setPos(x + 0.5, y + 1, z + 0.5)
 
-grid_start = (x, y - 1, z)
-grid_end = (x + LENGHT, y - 1 + HEIGHT, z + WIDTH)
-
-grid_blocks = []
-
-# outer loop for layers
-for y in range(HEIGHT + 1):
-    # now 2d grids
-    for x in range(LENGHT):
-        for z in range(WIDTH):
-            grid_blocks.append(mc.getBlock(grid_start[0] + x, grid_start[1] + y, grid_start[2] + z))
+grid_start = (int(x),int(y - 1), int(z))
+grid_end = (int(x + LENGHT), int(y - 1 + HEIGHT), int(z + WIDTH))
+grid_blocks = get_blocks(grid_start, grid_end)
 
 grid = np.resize(grid_blocks, (HEIGHT + 1, LENGHT, WIDTH))
 
@@ -42,33 +35,35 @@ layer_counter = 0
 
 if HEIGHT > 1:
     try:
-        os.mkdir(os.path.join("3d_mazes", maze_3d_name))
+        os.mkdir(os.path.join("3d_mazes", maze_3d_name))     
     except FileExistsError:
         print("Directory with that name already exists")
 
-for layer in grid:
-    df = pd.DataFrame(index=range(LENGHT), columns=range(WIDTH))
-    row_counter = 0
-    for row in layer:
-        block_counter = 0
-        for block_data in row:
-            # checking if block is AIR or not
-            if mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).id != 0:
-                # leaving it as wall for now, will change to correct weight in the future
-                df[block_counter][LENGHT - row_counter - 1] = '-'
-            if block_data == 251:
-                if mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).data == 5:
-                    df[block_counter][LENGHT - row_counter - 1] = 'S'
-                    start_node = (block_counter, LENGHT - row_counter - 1)
-                elif mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).data == 14:
-                    df[block_counter][LENGHT - row_counter - 1] = 'E'
-                    target_node = (block_counter, LENGHT - row_counter - 1)
-            block_counter += 1
-        row_counter += 1
+# TO BE CHANGED
+
+# for layer in grid:
+#     df = pd.DataFrame(index=range(LENGHT), columns=range(WIDTH))
+#     row_counter = 0
+#     for row in layer:
+#         block_counter = 0
+#         for block_data in row:
+#             # checking if block is AIR or not
+#             if mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).id != 0:
+#                 # leaving it as wall for now, will change to correct weight in the future
+#                 df[block_counter][LENGHT - row_counter - 1] = '-'
+#             if block_data == 251:
+#                 if mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).data == 5:
+#                     df[block_counter][LENGHT - row_counter - 1] = 'S'
+#                     start_node = (block_counter, LENGHT - row_counter - 1)
+#                 elif mc.getBlockWithData(grid_start[0] + row_counter, grid_start[1] + layer_counter, grid_start[2] + block_counter).data == 14:
+#                     df[block_counter][LENGHT - row_counter - 1] = 'E'
+#                     target_node = (block_counter, LENGHT - row_counter - 1)
+#             block_counter += 1
+#         row_counter += 1
     
-    df.fillna(0, inplace=True)
-    if HEIGHT > 1:
-        df.to_csv(os.path.join("3d_mazes", maze_3d_name, f"layer_{layer_counter}.csv"))
-    else:
-         df.to_csv(os.path.join("saved_mazes", f"{maze_2d_name}.csv"))
-    layer_counter += 1
+#     df.fillna(0, inplace=True)
+#     if HEIGHT > 1:
+#         df.to_csv(os.path.join("3d_mazes", maze_3d_name, f"layer_{layer_counter}.csv"))
+#     else:
+#          df.to_csv(os.path.join("saved_mazes", f"{maze_2d_name}.csv"))
+#     layer_counter += 1
