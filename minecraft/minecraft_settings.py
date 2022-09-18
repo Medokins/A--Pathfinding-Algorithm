17292406387df11910f17ca6919c6536972449cc
Medokins <nikodemszwast@gmail.com>
2022-09-18 15:39:46 +0200

    add BIAS for block sorting

--- minecraft/minecraft_settings.py
+++ minecraft/minecraft_settings.py
@@ -1,10 +1,14 @@
-WIDTH = 100
-LENGHT = 100
-HEIGHT = 20
+WIDTH = 20
+LENGHT = 20
+HEIGHT = 5
 
 START = (251, 5)
 END = (251, 14)
 WALL = (251, 15)
 PATH = (251, 3)
 MARGIN = 0.545
+
+# for maze sorting in 3d mazes, BIAS 1000 means it will work for mazes up to 1.000 x 1.000 in size 
+# and in x coordinate not passing 99.999 blocks
+BIAS = 1000
 set_paramiter = False
